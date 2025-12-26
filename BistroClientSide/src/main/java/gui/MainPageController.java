8d8238bf5caa35a7066a5716204f47f88a212d79
tomer levy <tomerlevy0404@gmail.com>
2025-12-26 11:33:03 +0200

    update the main page screen and add screens for client login and staff login

--- BistroClientSide/src/main/java/gui/MainPageController.java
+++ BistroClientSide/src/main/java/gui/MainPageController.java
@@ -10,51 +10,34 @@ import javafx.scene.Parent;
 
 public class MainPageController {
 
-	@FXML
-	private void openGuestUI(ActionEvent event) {
-	    openUI(event, "GuestLoginGUI.fxml", "Guest Login");
-	}
+    @FXML
+    private void openClientOptions(ActionEvent event) {
+        openUI(event, "ClientLoginOptions.fxml", "Client Options");
+    }
 
-	@FXML
-	private void openMemberUI(ActionEvent event) {
-	    openUI(event, "MemberLoginGUI.fxml", "Member Login");
-	}
-
-	@FXML
-	private void openStaffUI(ActionEvent event) {
-	    openUI(event, "StaffLoginGUI.fxml", "Staff Login");
-	}
-
-	@FXML
-	private void openManagerUI(ActionEvent event) {
-	    openUI(event, "ManagerLoginGUI.fxml", "Manager Login");
-	}
-
+    @FXML
+    private void openStaffOptions(ActionEvent event) {
+        openUI(event, "StaffLoginOptions.fxml", "Staff Options");
+    }
 
     private void openUI(ActionEvent event, String fxmlFile, String title) {
         try {
-            Parent root = FXMLLoader.load(
-                    getClass().getResource("/gui/" + fxmlFile)
-            );
-
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/" + fxmlFile));
             Stage stage = new Stage();
-            stage.setTitle(title + " Screen");
+            stage.setTitle(title);
             stage.setScene(new Scene(root));
             stage.show();
-
             ((Node) event.getSource()).getScene().getWindow().hide();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-	public void start(Stage primaryStage) throws Exception {	
-		Parent root = FXMLLoader.load(getClass().getResource("/gui/mainPage.fxml"));
-				
-		Scene scene = new Scene(root);
-		primaryStage.setTitle("Main Page");
-		primaryStage.setScene(scene);
-		
-		primaryStage.show();	 	   
-	}
+    
+    public void start(Stage primaryStage) throws Exception {    
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/MainPage.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Main Page");
+        primaryStage.setScene(scene);
+        primaryStage.show();           
+    }
 }
